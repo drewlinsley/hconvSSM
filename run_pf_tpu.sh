f821de2731a320fde4a0e7e26c71f0474f29a506
Drew Linsley <drewlinsley@gmail.com>
2024-03-24 10:23:23 -0400

    Fixed TPU saving.

--- run_pf_tpu.sh
+++ run_pf_tpu.sh
@@ -1,3 +1,3 @@
 rm -rf logs/*
-python scripts/train_pf.py -d pathfinder -o hpf-14 -c configs/PF-14/tpu-hPF-14.yaml
+python scripts/train_pf.py -d pathfinder -o gs://serrelab/hssm/hpf-14 -c configs/PF-14/hPF-14.yaml
 
