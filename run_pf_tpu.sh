0070be406070f43f01a20e6dd743339a93a67a0f
Drew Linsley <drewlinsley@gmail.com>
2024-04-09 13:42:39 -0400

    Changed default tpu job.

--- run_pf_tpu.sh
+++ run_pf_tpu.sh
@@ -1,3 +1,3 @@
 rm -rf logs/*
-python scripts/train_pf.py -d pathfinder -o gs://serrelab/hssm/hpf-14-8 -c configs/PF-14/hPF-14.yaml
+python scripts/train_pf.py -d pathfinder -o gs://serrelab/hssm/hpf-14-10 -c configs/PF-14/hPF-14.yaml
 
